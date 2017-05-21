c5dc3b8afa9bc695940e8f767391d9ddf5f5c67a
MilosSavic <savic.milan@sezampro.rs>
2017-05-22 00:49:04 +0200

    Merge

--- Admin app/models/airline.server.model.js
+++ Admin app/models/airline.server.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by Nemanja on 5/21/2017.
  */
@@ -18,24 +17,3 @@ const Airline = sequelize.define('Airline', {
 function getAirline(){
     return Airline;
 }
-=======
-/**
- * Created by Nemanja on 5/21/2017.
- */
-
-var Sequelize = require('sequelize');
-module.exports.getAirline = getAirline;
-
-const Airline = sequelize.define('Airline', {
-    Name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}, {
-    freezeTableName: true // Model tableName will be the same as the model name
-});
-
-function getAirline(){
-    return Airline;
-}
->>>>>>> 421ad6a34e5dc4f7b4af00d363faa6e56d54b8bb
