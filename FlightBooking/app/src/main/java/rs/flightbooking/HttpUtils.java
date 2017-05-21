bbc6cf5a0ec33b05bdd574e0403b739c540b37ca
MilosSavic <savic.milan@sezampro.rs>
2017-05-21 15:54:31 +0200

    Branch rename

--- FlightBooking/app/src/main/java/rs/flightbooking/HttpUtils.java
+++ FlightBooking/app/src/main/java/rs/flightbooking/HttpUtils.java
@@ -9,7 +9,7 @@ import com.loopj.android.http.RequestParams;
  */
 
 public class HttpUtils {
-    private static final String BASE_URL = "BASE URL - type something here";
+    private static final String BASE_URL = "BASE URL - type something here...";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
