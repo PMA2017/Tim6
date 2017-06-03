bf8f936b70c71d0353e9a8edb935c9cff3b9a8a7
MilosSavic <savic.milan@sezampro.rs>
2017-06-03 15:57:04 +0200

    Generic PUT and DELETE

--- Admin app/controllers/generic.server.controller.js
+++ Admin app/controllers/generic.server.controller.js
@@ -8,6 +8,8 @@ Sequelize = require('sequelize');
 module.exports.list = list;
 module.exports.getById = getById;
 module.exports.postToTable = postToTable;
+module.exports.putToTable = putToTable;
+module.exports.delete = deleteFromTable;
 
 function list(req, res, next, tableName){
 	sequelize.model(tableName).findAll()
@@ -42,3 +44,37 @@ function postToTable(req,res,next){
 	})
 }
 
+function putToTable(req,res,next){
+	var tableNameId = req.params.tableToPut;
+	var rowId = req.body.id;
+	sequelize.model(tableNameId).findById(rowId)
+	  .then(data=>{
+	  	if(data){
+	  		data.update(req.body).then(updatedInstance=>{
+	  			res.status(200).send(updatedInstance);
+	  		})
+	  		.catch(error => {
+	  			res.status(400).send({message:error});
+	  		})
+	  	}
+	  }).catch(error=>{
+	  	res.status(400).send({message:error});
+	  })
+}
+
+function deleteFromTable(req,res,next){
+	var rowId = req.params.deleteRowId;
+	var tableNameId = req.params.tableToDeleteFrom;
+
+	sequelize.model(tableNameId).findById(rowId)
+		.then(data=>{
+						data.destroy({force:true}).then(()=>{
+						res.status(200).send({message:"Instance successfully deleted"});
+					}).catch(error=>{
+						res.status(400).send({message:error});
+					})
+				
+			}).catch(error=>{
+			res.status(400).send({message:error});
+		})
+}
