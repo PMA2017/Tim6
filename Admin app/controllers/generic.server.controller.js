e22c38025ff4936211de13fb1156c511adb55931
MilosSavic <savic.milan@sezampro.rs>
2017-06-15 02:16:40 +0200

    Get flights around date (not yet finished)

--- Admin app/controllers/generic.server.controller.js
+++ Admin app/controllers/generic.server.controller.js
@@ -10,6 +10,7 @@ module.exports.getById = getById;
 module.exports.postToTable = postToTable;
 module.exports.putToTable = putToTable;
 module.exports.delete = deleteFromTable;
+module.exports.getDrivesAroundDate = getDrivesAroundDate;
 
 function list(req, res, next, tableName){
 	sequelize.model(tableName).findAll()
@@ -78,3 +79,80 @@ function deleteFromTable(req,res,next){
 			res.status(400).send({message:error});
 		})
 }
+
+//https://pastebin.com/qFfn7cCX
+function getDrivesAroundDate(req,res,next){
+	var cityFrom = req.body.MarkTownFrom;
+	var cityTo = req.body.MarkTownTo;
+	var dateFrom = new Date(req.body.DateFrom);
+	var dateTo = new Date(req.body.DateTo);
+	var passengers = req.body.Passengers;
+	var cityFromId;
+	var cityToId;
+	var airportsFrom;
+	var airportsTo;
+	sequelize.model('Town').findOne({where:{Mark:cityFrom}}).then(townFrom=>{
+		cityFromId = townFrom.id;
+		sequelize.model('Town').findOne({where:{Mark:cityTo}}).then(townTo=>{
+			cityToId = townTo.id;
+			sequelize.model('Airport').findAll({where:{Town_ID:cityFromId}}).then(airportsFrom=>{
+					var queryFrom = [];
+					for(var i=0; i<airportsFrom.length; i++)
+					{
+						queryFrom.push({AirportFrom_ID:airportsFrom[i].id});
+					}
+					sequelize.model('Airport').findAll({where:{Town_ID:cityToId}}).then(airportsTo=>{
+						var queryTo = [];
+						for(var i=0; i<airportsTo.length; i++){
+							queryTo.push({AirportTo_ID:airportsTo[i].id});
+						}
+						sequelize.model('Flight').findAll({where:Sequelize.and(Sequelize.or(queryTo),Sequelize.or(queryFrom))}).then(flights=>{
+							
+							var queryFlights = [];
+							for(var i=0; i<flights.length; i++){
+								queryFlights.push({Flight_ID:flights[i].id});
+							}
+							var queryDate = [];
+							var dateFromMin = new Date(dateFrom);
+							dateFromMin.setDate(dateFromMin.getDate()-2);
+							dateFromMin.setHours(0);
+							dateFromMin.setMinutes(0);
+							dateFromMin.setSeconds(0);
+							var dateFromMax = new Date(dateFrom);
+							dateFromMax.setDate(dateFromMax.getDate()+3);
+							dateFromMax.setHours(0);
+							dateFromMax.setMinutes(0);
+							dateFromMax.setSeconds(0);
+							var dateToMin = new Date(dateTo);
+							dateToMin.setDate(dateToMin.getDate()-2);
+							dateToMin.setHours(0);
+							dateToMin.setMinutes(0);
+							dateToMin.setSeconds(0);
+							var dateToMax = new Date(dateTo);
+							dateToMax.setDate(dateToMax.getDate()+3);
+							dateToMax.setHours(0);
+							dateToMax.setMinutes(0);
+							dateToMax.setSeconds(0);
+
+							var testDate = new Date(2017,6,15);
+
+							console.log(dateFromMin);
+							console.log(dateFromMax);
+							console.log(dateToMin);
+							console.log(dateToMax);
+
+
+							sequelize.model('Drive').findAll({where:Sequelize.and(Sequelize.or(queryFlights),Sequelize.or({StartTime:{$gte:dateFromMin, $lte:dateFromMax}},{StartTime:{$gte:dateToMin, $lte:dateToMax}}))}).then(drives=>{
+								res.json(drives);
+								return;
+								res.json(drives);
+							})
+						})
+					})
+					
+			})
+		})
+	})
+}
+
+
